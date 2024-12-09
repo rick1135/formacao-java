121fcf0c79b80f2f2714430faeda4b407cb4ef65
rick1135 <rick1135@hotmail.com>
2024-12-09 19:18:05 -0300

    operadores

---
+++ Formação Java/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,55 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        // String concatenacao = "?";
+        // concatenacao = 1+1+1+"1";
+        // System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+1;
+        // System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+"1";
+        // System.out.println(concatenacao);
+        // concatenacao = "1"+1+1+1;
+        // System.out.println(concatenacao);
+        // concatenacao = "1"+(1+1+1);
+        // System.out.println(concatenacao);
+
+        // int numero = 5;
+        // numero = -numero;
+        // System.out.println(numero);        
+        // numero = + numero;
+        // System.out.println(numero);
+        // numero = numero * -1;
+        // System.out.println(numero);
+        // int numero = 5;
+        // numero = numero + 1;
+        // System.out.println(++numero);
+
+        // int a= 5, b = 10;
+        // String resultado = a==b ? "verdadeiro" : "falso";
+        // System.out.println(resultado);
+
+        // int n1 =1, n2 = 2;
+        // boolean SN = n1 == n2;
+        // System.out.println(SN);
+        // SN = n1 != n2;
+        // System.out.println(SN);
+        // if (n1>n2) {
+        //     System.out.println("n1 é maior que n2");
+        // } else {
+        //     System.out.println("n1 não é maior que n2");
+        // }
+        // String nomea = "João", nomeb = "Maria";
+        // System.out.println(nomea.equals(nomeb));
+
+        boolean a = true, b = false;
+        if (a && b) {
+            System.out.println("a e b são verdadeiros");
+        } else {
+            System.out.println("a e b não são verdadeiros");
+        }
+        if (a || b) {
+            System.out.println("a ou b são verdadeiros");
+        } else {
+            System.out.println("a ou b não são verdadeiros");
+        }
+    }
+}
--- Formação Java/tipos-variaveis/src/TiposVariaveis.java
+++ Formação Java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = (short)numeroCurto;
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        final double PI = 3.14;
+        // PI = 3.14159; // Erro
     }
 }
