5f0e7e61e24bbbd8921c4fa8e77b8d1b221c2cd5
rick1135 <rick1135@hotmail.com>
2025-04-11 20:28:00 -0300

    spring boot security

--- Formação Java/spring-security/spring-security/src/main/java/com/spring_security/spring_security/WebSecurityConfig.java
+++ Formação Java/spring-security/spring-security/src/main/java/com/spring_security/spring_security/WebSecurityConfig.java
@@ -1,4 +1,24 @@
 package com.spring_security.spring_security;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}325158")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}adm325158")
+                .roles("MANAGERS");
+    }
 }
