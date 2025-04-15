b610360d27ef47447072a873a8e20d00b069baff
rick1135 <rick1135@hotmail.com>
2025-04-14 21:31:21 -0300

    security

---
+++ Formação Java/spring-security/spring-security/src/main/java/com/spring_security/spring_security/config/SecurityDatabaseService.java
@@ -0,0 +1,4 @@
+package com.spring_security.spring_security.config;
+
+public class SecurityDatabaseService {
+}
---
+++ Formação Java/spring-security/spring-security/src/main/java/com/spring_security/spring_security/model/User.java
@@ -0,0 +1,4 @@
+package com.spring_security.spring_security.model;
+
+public class User {
+}
