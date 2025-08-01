344a6da3681b6ac0616e7c88c6c59de0bb6b8721
Alberto Henrique Mauricio Ribeiro <55491414+rick1135@users.noreply.github.com>
2025-07-31 22:39:23 -0300

    finalizando implementação de Professor

---
+++ Formação Java/projeto-final-academia-digital/src/main/java/me/dio/academia/digital/entity/form/ProfessorUpdateForm.java
@@ -0,0 +1,11 @@
+package me.dio.academia.digital.entity.form;
+
+import java.time.LocalDate;
+
+public class ProfesorUpdateForm {
+    private String nome;
+
+    private String bairro;
+
+    private LocalDate dataDeNascimento;
+}
